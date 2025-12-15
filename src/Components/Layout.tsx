da2ad721f308c86b91317fe829c4b5813f45523d
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-15 16:58:54 +0530

    created alayout component

---
+++ src/Components/Layout.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
+import Student from './student';
+import Course from './course';
+import Enrollment from './enrollment';
+// import Login from './login';
+
+export default function Main() {
+  return (
+    <Router>
+      {/* <Login /> */}
+      <NavBar />  
+      <div className="container mt-3">  
+        <Routes>
+          <Route path="/students" element={<Student />} />  
+          <Route path="/courses" element={<Course />} /> 
+          <Route path="/enrollments" element={<Enrollment />} />  
+          <Route path="/" element={<Student />} />
+        </Routes>
+      </div>
+   
+    </Router>
+  );
+}
