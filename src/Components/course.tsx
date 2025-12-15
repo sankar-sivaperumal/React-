f60bf2a2aa6687d2917b6708e612f3fe44b7ab96
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-15 16:58:28 +0530

    Created new component courses

---
+++ src/Components/course.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+
+interface Courses {
+    course_id: number;
+    course_name: string;
+    teacher_name: string;
+}
+
+function Courses() {
+  const [data, setData] = useState<Courses[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/courses/")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching course data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>Courses</h2>
+      
+      <table border={2}>
+        <thead>
+          <tr>
+            <th>Course ID</th>
+            <th>Course Name</th>
+            <th>Teacher Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((course) => (
+            <tr key={course.course_id}>
+              <td>{course.course_id}</td>
+              <td>{course.course_name}</td>
+              <td>{course.teacher_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Courses;
