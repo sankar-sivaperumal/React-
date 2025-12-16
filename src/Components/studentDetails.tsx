ef9f65ac448b351f3557f96d3d562438197fcb2b
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-16 14:38:53 +0530

    created new component for studentsdetails

---
+++ src/Components/studentDetails.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+
+interface Student {
+  student_id: number;
+  name: string;
+  age: number;
+  gender: string;
+  city: string;
+  date_of_birth: string;
+  files: string[];
+}
+
+interface Enrollment {
+  enrollment_id: number;
+  marks: string;
+  students: Student;
+}
+
+interface Course {
+  course_id: number;
+  course_name: string;
+  teacher_name: string;
+  enrollments: Enrollment[];
+}
+
+function studentDetails() {
+  const [data, setData] = useState<Course[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/courses/")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching course data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2 style={{textAlign:"center"}}>Student Records</h2>
+
+      {data.map((course) => (
+        <div key={course.course_id}>
+          <table border={2}>
+            <thead>
+              <tr>
+                <th colSpan={7} style={{ textAlign: "center" }}>
+                  {course.course_name} - <span>{course.teacher_name}</span><br></br>
+                  <span>Course ID: {course.course_id}</span>
+                </th>
+              </tr>
+              <tr>
+                <th>Enrollment ID</th>
+                <th>Student Name</th>
+                <th>Marks</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>City</th>
+                <th>Date of Birth</th>
+              </tr>
+            </thead>
+            <tbody>
+              {course.enrollments.map((enrollment) => (
+                <tr key={enrollment.enrollment_id}>
+                  <td>{enrollment.enrollment_id}</td>
+                  <td>{enrollment.students.name}</td>
+                  <td>{enrollment.marks}</td>
+                  <td>{enrollment.students.age}</td>
+                  <td>{enrollment.students.gender}</td>
+                  <td>{enrollment.students.city}</td>
+                  <td>{enrollment.students.date_of_birth}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default studentDetails;
