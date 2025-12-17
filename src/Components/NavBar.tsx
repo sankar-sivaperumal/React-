32985d01d9b9f77b78f3959eab4ce23f5d636724
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-17 17:29:42 +0530

    updated new componenets

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+/* import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -10,10 +10,63 @@ function NavBar() {
           <li><Link className="nav-link" to="/courses">Courses</Link></li>
           <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
           <li><Link className="nav-link" to="/studentDetails">StudentRecords</Link></li>
+          <li><Link className="nav-link" to="/formslayout/FormPage">FormPage</Link></li>
         </ul>
       </div>
     </nav>
   </>
+  );
+}
+
+export default NavBar;
+ */
+
+
+/* import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-sm bg-primary text-color-black">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li><Link className="nav-link" to="/students">Students</Link></li>
+          <li><Link className="nav-link" to="/courses">Courses</Link></li>
+          <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
+          <li><Link className="nav-link" to="/studentDetails">Student Records</Link></li>
+         
+          <li><Link className="nav-link" to="/formslayout/Formpage">Form Page</Link></li>  
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
+ */
+
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-sm bg-primary text-color-black">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          {/* Existing Links */}
+          <li><Link className="nav-link" to="/students">Students</Link></li>
+          <li><Link className="nav-link" to="/courses">Courses</Link></li>
+          <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
+          <li><Link className="nav-link" to="/student-details">Student Records</Link></li>
+          
+          {/* New Links */}
+          <li><Link className="nav-link" to="/signup">Signup</Link></li>  
+          <li><Link className="nav-link" to="/login">Login</Link></li>   
+        
+          
+          {/* Update for FormsLayout */}
+          <li><Link className="nav-link" to="/formslayout/Formpage">Form Page</Link></li>  
+        </ul>
+      </div>
+    </nav>
   );
 }
 
