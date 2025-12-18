6b889687b022ef433e88e3619fac00e070910a5a
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-18 14:42:22 +0530

    updated new components

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,161 +1,82 @@
-/* /* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import Student from './student';
-import Course from './course';
-import Enrollment from './enrollment';
-import StudentDeatils from './studentDetails';
-import FormsLayout from './Forms/formslayout';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Signup from "./Signup";
+import Login from "./login";
+import Student from "./student";
+import Courses from "./course";
+import Enrollments from "./enrollment";
+import StudentDetails from "./studentDetails";
+import { FormProvider } from "./Forms/formcontext";
+import { AuthProvider } from "../Components/Access/AuthContext";
+import ProtectedRoute from "./Access/FlowGuard";
+import FormPage from "./Forms/Formpage";
 
-// import Login from './login';
-
-export default function Main() {
+const Main: React.FC = () => {
   return (
     <Router>
-     
-      <NavBar />  
-      <div className="container mt-3">  
-        <Routes>
-          <Route path="/students" element={<Student />} />  
-          <Route path="/courses" element={<Course />} /> 
-          <Route path="/enrollments" element={<Enrollment />} />  
-          <Route path="/studentDetails" element={<StudentDeatils />} />
-          <Route path="/formslayout" element={<FormsLayout />} />
-        </Routes>
-      </div>
-  
+      <AuthProvider>
+        <NavBar />
+
+        <div className="container mt-3">
+          <Routes>
+            
+            <Route path="/" element={<Navigate to="/signup" />} />
+
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+
+           
+            <Route
+              path="/Formpage/*"
+              element={
+                <ProtectedRoute>
+                  <FormProvider>
+                    <FormPage />
+                  </FormProvider>
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/student"
+              element={
+                <ProtectedRoute>
+                  <Student />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/enrollments"
+              element={
+                <ProtectedRoute>
+                  <Enrollments />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/courses"
+              element={
+                <ProtectedRoute>
+                  <Courses />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/student-details"
+              element={
+                <ProtectedRoute>
+                  <StudentDetails />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </AuthProvider>
     </Router>
   );
-} */
+};
 
-/* import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './NavBar';
-import Student from './student';
-import Course from './course';
-import Enrollment from './enrollment';
-import StudentDetails from './studentDetails';
-import FormsLayout from './Forms/formslayout';
-
-export default function Main() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/students" element={<Student />} />
-          <Route path="/courses" element={<Course />} />
-          <Route path="/enrollments" element={<Enrollment />} />
-          <Route path="/studentDetails" element={<StudentDetails />} />
-          <Route path="/formslayout/*" element={<FormsLayout />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
- */
-
-/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import Student from './student';
-import Course from './course';
-import Enrollment from './enrollment';
-import StudentDetails from './studentDetails';
-import FormsLayout from './Forms/formslayout';
-import NextPage from './Forms/NextPage'; 
-
-export default function Main() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/students" element={<Student />} />
-          <Route path="/courses" element={<Course />} />
-          <Route path="/enrollments" element={<Enrollment />} />
-          <Route path="/studentDetails" element={<StudentDetails />} />
-          <Route path="/formslayout/*" element={<FormsLayout />} />
-          <Route path="/NextPage" element={<NextPage />} /> 
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-  */
-
-
-
-/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import Student from './student';
-import Course from './course';
-import Enrollment from './enrollment';
-import StudentDetails from './studentDetails';
-import FormsLayout from './Forms/formslayout';
-import { FormProvider } from './Forms/formcontext'; 
-
-export default function Main() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/students" element={<Student />} />
-          <Route path="/courses" element={<Course />} />
-          <Route path="/enrollments" element={<Enrollment />} />
-          <Route path="/studentDetails" element={<StudentDetails />} />
-          
-    
-          <Route 
-            path="/formslayout/*" 
-            element={
-              <FormProvider>
-                <FormsLayout />
-              </FormProvider>
-            } 
-          />
-
-        
-        </Routes>
-      </div>
-    </Router>
-  );
-}
- */
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import Signup from './Signup';  
-import Login from './login';    
-
-import Students from './student';       
-import Courses from './course';         
-import Enrollments from './enrollment'; 
-import StudentDetails from './studentDetails';  
-import FormsLayout from './Forms/formslayout'; 
-import { FormProvider } from './Forms/formcontext'; 
-
-export default function Main() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="container mt-3">
-        <Routes>
-         
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/students" element={<Students />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/enrollments" element={<Enrollments />} />
-          <Route path="/student-details" element={<StudentDetails />} />
-          <Route 
-            path="/formslayout/*" 
-            element={
-              <FormProvider>
-                <FormsLayout />
-              </FormProvider>
-            } 
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+export default Main;
