1a491a1cae1d50fc262e33f03b341ee75814fb97
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-18 14:43:15 +0530

    updated new components

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,69 +1,66 @@
-/* import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useAuth } from "./Access/AuthContext";
 
 function NavBar() {
+  const { isLoggedIn } = useAuth();
+
   return (
-    <>
-      <nav className="navbar navbar-expand-sm bg-primary text-color-black">
-      <div className="container-fluid">
-        <ul className="navbar-nav ">
-          <li><Link className="nav-link" to="/students">Students</Link></li>
-          <li><Link className="nav-link" to="/courses">Courses</Link></li>
-          <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
-          <li><Link className="nav-link" to="/studentDetails">StudentRecords</Link></li>
-          <li><Link className="nav-link" to="/formslayout/FormPage">FormPage</Link></li>
-        </ul>
-      </div>
-    </nav>
-  </>
-  );
-}
-
-export default NavBar;
- */
-
-
-/* import { Link } from 'react-router-dom';
-
-function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-sm bg-primary text-color-black">
+    <nav className="navbar navbar-expand-sm bg-primary">
       <div className="container-fluid">
         <ul className="navbar-nav">
-          <li><Link className="nav-link" to="/students">Students</Link></li>
-          <li><Link className="nav-link" to="/courses">Courses</Link></li>
-          <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
-          <li><Link className="nav-link" to="/studentDetails">Student Records</Link></li>
+
          
-          <li><Link className="nav-link" to="/formslayout/Formpage">Form Page</Link></li>  
-        </ul>
-      </div>
-    </nav>
-  );
-}
+          {!isLoggedIn && (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/signup">
+                  Signup
+                </Link>
+              </li>
 
-export default NavBar;
- */
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/login">
+                  Login
+                </Link>
+              </li>
+            </>
+          )}
 
-import { Link } from 'react-router-dom';
-
-function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-sm bg-primary text-color-black">
-      <div className="container-fluid">
-        <ul className="navbar-nav">
-          {/* Existing Links */}
-          <li><Link className="nav-link" to="/students">Students</Link></li>
-          <li><Link className="nav-link" to="/courses">Courses</Link></li>
-          <li><Link className="nav-link" to="/enrollments">Enrollments</Link></li>
-          <li><Link className="nav-link" to="/student-details">Student Records</Link></li>
           
-          {/* New Links */}
-          <li><Link className="nav-link" to="/signup">Signup</Link></li>  
-          <li><Link className="nav-link" to="/login">Login</Link></li>   
-        
-          
-          {/* Update for FormsLayout */}
-          <li><Link className="nav-link" to="/formslayout/Formpage">Form Page</Link></li>  
+          {isLoggedIn && (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/Formpage">
+                  Form Page
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/students">
+                  Students
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/enrollments">
+                  Enrollments
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/courses">
+                  Courses
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/student-details">
+                  Student Records
+                </Link>
+              </li>
+            </>
+          )}
+
         </ul>
       </div>
     </nav>
