1860bae5f8b50b69b0cb9b194cd96b3b2d7f53c3
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-19 17:54:20 +0530

    added toasted to display confrom message in delete and edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Main from "./Components/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App">
-     
       <Main />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
     </div>
   );
 }
