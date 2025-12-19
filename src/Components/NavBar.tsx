39171e716dedda1e8ed71a4a738147b559600eb8
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-19 13:22:14 +0530

    updated navbar with mobile view

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,66 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./Access/AuthContext";
 
 function NavBar() {
   const { isLoggedIn } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="navbar navbar-expand-sm bg-primary">
-      <div className="container-fluid">
-        <ul className="navbar-nav">
+    <nav className="navbar">
+      <div className="nav-container">
+  
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
 
-         
+        <ul className={`nav-links ${isOpen ? "open" : ""}`}>
           {!isLoggedIn && (
             <>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/signup">
-                  Signup
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/login">
-                  Login
-                </Link>
-              </li>
+              <li><Link to="/signup">Signup</Link></li>
+              <li><Link to="/login">Login</Link></li>
             </>
           )}
 
-          
           {isLoggedIn && (
             <>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/Formpage">
-                  Form Page
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/students">
-                  Students
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/enrollments">
-                  Enrollments
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/courses">
-                  Courses
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/student-details">
-                  Student Records
-                </Link>
-              </li>
+              <li><Link to="/Formpage">Form Page</Link></li>
+              <li><Link to="/students">Students</Link></li>
+              <li><Link to="/enrollments">Enrollments</Link></li>
+              <li><Link to="/courses">Courses</Link></li>
+              <li><Link to="/student-details">Student Records</Link></li>
             </>
           )}
-
         </ul>
       </div>
     </nav>
