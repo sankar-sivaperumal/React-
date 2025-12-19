2b63f75131df6befa793b531e851108d7b2543ac
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-19 13:11:10 +0530

    removed unwanted component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import Main from "./Components/Layout";
-// import Login from "./Components/login";
+
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
       <Main />
     </div>
   );
