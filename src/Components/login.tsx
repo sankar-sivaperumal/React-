1237d5e113052fa6c12dacc78568b42768cd82d2
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-19 11:41:41 +0530

    remove unwanted codes and spaces

--- src/Components/login.tsx
+++ src/Components/login.tsx
@@ -1,25 +1,3 @@
-/* 
-function Login(){
-
- return (  
-
-<form  className = "my-5"style={{width:"25%",margin:"auto"}}>
-    <h1 style={{textAlign:"center"}}>Login</h1>
-  <div className="mb-3">
-    <label  className="form-label">Email address</label>
-    <input type="email" className="form-control"  />
-  </div>
-  <div className="mb-3" >
-    <label  className="form-label">Password</label>
-    <input   type="password" className="form-control" />
-  </div>
-  <button type="submit" className="btn btn-primary">Login</button>
-</form>
-    ); }
-
-export default Login; */
-
-
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./Access/AuthContext";
 
