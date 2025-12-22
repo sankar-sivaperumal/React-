5038d02b8253fcd136ff67db387138ac51a7fbaf
Sankar-Sivaperumal <sankar.sivaperumal@datasirpi.com>
2025-12-22 10:39:29 +0530

    updated signup and login with same details

--- src/Components/Access/FlowGuard.tsx
+++ src/Components/Access/FlowGuard.tsx
@@ -1,14 +1,25 @@
 import React, { type JSX } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import { toast } from "react-toastify";
 
 type Props = {
   children: JSX.Element;
 };
 
 const ProtectedRoute: React.FC<Props> = ({ children }) => {
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? children : <Navigate to="/login" replace />;
+  const { isSignedUp, isLoggedIn } = useAuth();
+
+  if (!isSignedUp) {
+    toast.error("Please sign up first");
+    return <Navigate to="/signup" replace />;
+  }
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
